6387e6c81fa3019a23fe4e4480bd168bc5becd01
Leejun14 <777ljy777@gmail.com>
2026-01-22 09:54:08 +0900

    Feat: x만큼 간격이 있는 n개의 숫자

---
+++ src/lv2/IncreaseBy.java
@@ -0,0 +1,14 @@
+package lv2;
+
+import java.util.Arrays;
+
+public class IncreaseBy {
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+
+        for (int i = 1; i <= n; i++) {
+            answer[i-1] = ((long) x * i);
+        }
+        return answer;
+    }
+}
