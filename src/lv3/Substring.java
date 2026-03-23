2c5a9417facfd2febbd35558531f2b8e01a8ea80
Leejun14 <777ljy777@gmail.com>
2026-03-23 09:32:04 +0900

    feat: 크기가 작은 부분 문자열

---
+++ src/lv3/Substring.java
@@ -0,0 +1,17 @@
+package lv3;
+
+public class Substring {
+    public int solution(String t, String p) {
+        int answer = 0;
+        Long pLong = Long.parseLong(p);
+
+        for (int i = 0; i < t.length() - p.length() + 1;  i++) {
+            String s = t.substring(i, i + p.length());
+
+            if (Long.parseLong(s) <= pLong) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
