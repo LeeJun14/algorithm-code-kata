4d8c52a15ce25daf85a3f8161528ecb300bc78b5
LeeJun14 <777ljy777@gmail.com>
2026-04-16 10:00:45 +0900

    feat: 푸드 파이트 대회

---
+++ src/lv3/FoodFight.java
@@ -0,0 +1,16 @@
+package lv3;
+
+public class FoodFight {
+    public String solution(int[] food) {
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < food.length; i++) {
+            int count = food[i] / 2;
+            sb.append(String.valueOf(i).repeat(count));
+        }
+
+        String answer = sb + "0";
+        answer += sb.reverse();
+        return answer;
+    }
+}
--- src/lv3/NearestSameCharacter.java
+++ src/lv3/NearestSameCharacter.java
@@ -1,4 +1,21 @@
 package lv3;
 
 public class NearestSameCharacter {
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i; j >= 0; j--) {
+                if (s.charAt(i) == s.charAt(j) && answer[i] == 0) {
+                    answer[i] = i - j;
+
+                }
+            }
+            if (answer[i] == 0) {
+                answer[i] = -1;
+            }
+        }
+
+        return answer;
+    }
 }
